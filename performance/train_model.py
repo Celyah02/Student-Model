9c5b055689ab356dcc7dafdda0f89e7a18468b54
FURAHA NIYONGIRA Celia <furahacelia33@gmail.com>
2026-01-26 17:14:26 +0200

    first commit

---
+++ performance/train_model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+
+def train():
+    df = pd.read_csv('dataset.csv')
+
+    # Encode categorical column
+    encoder = LabelEncoder()
+    df['Extracurricular Activities'] = encoder.fit_transform(
+        df['Extracurricular Activities']
+    )
+
+    X = df.drop('Performance Index', axis=1)
+    y = df['Performance Index']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = RandomForestRegressor(
+        n_estimators=200,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    joblib.dump(model, 'performance/model.pkl')
+    print("Model trained and saved successfully.")
---
+++ student_ml/settings.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'performance',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',   # REQUIRED
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', # REQUIRED
+    'django.contrib.messages.middleware.MessageMiddleware',    # REQUIRED
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # OK even if folder doesn't exist yet
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
