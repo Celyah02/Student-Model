f35d6594ad4bd87ee612fd6d4568d82948c122a9
FURAHA NIYONGIRA Celia <furahacelia33@gmail.com>
2026-02-04 23:56:33 +0200

    Register API routes for performance prediction

--- student_ml/urls.py
+++ student_ml/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from performance.views import predict_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', predict_page, name='home'),
     path('api/', include('performance.urls')),
 ]
